1ddbd9d3700ce425b96af23cbe512ce1a7d06947
Ritik Varshney <ritikvarshney2014@gmail.com>
2025-09-08 23:37:19 +0530

    folder restructured

--- DSA/Tree/IsIdentical.js
+++
@@ -1,8 +0,0 @@
-// Problem Statement - https://leetcode.com/problems/same-tree/description/
-function isSameTree(p, q) {
-    if (p === null && q === null) return true;
-    if (p === null || q === null) return false;
-    if (p.val !== q.val) return false;
-
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-}
--- DSA/Tree/MaxDepth.js
+++
@@ -1,7 +0,0 @@
-function maxDepth(root) {
-    if(root === null) return 0;
-    let leftHeight = maxDepth(root.left);
-    let rightHeight = maxDepth(root.right);
-
-    return 1 + Math.max(leftHeight, rightHeight);
-}
